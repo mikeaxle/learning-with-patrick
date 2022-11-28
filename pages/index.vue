74d5d1047a7da0053b900b4c1ca2d34336940fc1
Gauthier Kanku <42734260+gauthierjorbel22@users.noreply.github.com>
2022-11-28 18:45:01 +0200

    add more sections and styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,32 @@
 <template>
   <main>
     <div class="parallax">
+      <div class="flex-container navbar">
+        <div class="flex-item-left text-left">Learn With Patrick</div>
+        <div class="flex-item-right">Product Manager</div>
+      </div>
+      <div class="container">
+        <div class="flex-container-head">
+          <div class="flex-item-left text-left">
+            <div class="display-4 m-5 text-left">
+              We are a creative group of people who design influential brands
+              and digital experiences.
+              <div class="button pt-5">
+                <span class="btn btn-success m-3">Learn More</span>
+                <span class="btn btn-success">Learn More</span>
+              </div>
+            </div>
+          </div>
+          <div class="flex-item-right">Hello</div>
+        </div>
+      </div>
     </div>
-
     <div
       class="bg-primary p-5"
       style="min-height: 800px; height: 100%; background-color: #00bfff"
     >
-      <div class="container">
-        <h1 class="text-center" style="font-size: 35px">We Are Glint</h1>
+      <div class="container text-center">
+        <h1 style="font-size: 35px">We Are Glint</h1>
         <p class="p-5 text-center text-dark" style="font-size: 25px">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
           deleniti illo alias harum fugiat nobis saepe tempore nemo sequi
@@ -45,41 +63,74 @@
     </div>
     <div class="parallax">
       <div class="container">
-        <h2 class="text-center">We've got everything you need to launch and grow your business</h2>
+        <h2 class="text-center">
+          We've got everything you need to launch and grow your business
+        </h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto
           adipisci alias quibusdam eos tenetur, reprehenderit facilis natus
           totam quae!
-        
         </p>
         <div class="flex-container-services">
-          <div class="flex-item-left">
-Hello world
-          </div>
-          <div class="flex-item-right">
-            Hello world
-          </div>
-          <div class="flex-item-right">
-            Hello world
-          </div>
-          <div class="flex-item-right">
-            Hello world
-          </div>
+          <div class="flex-item-left">Hello world</div>
+          <div class="flex-item-right">Hello world</div>
+          <div class="flex-item-right">Hello world</div>
+          <div class="flex-item-right">Hello world</div>
         </div>
       </div>
     </div>
     <div
       class="text-dark"
-      style="min-height: 800px; height: 100%; background-color: white"
+      style="min-height: 300px; height: 100%; background-color: white"
     >
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, quisquam
-        debitis hic est sit voluptatibus earum temporibus reprehenderit quas
-        praesentium suscipit corrupti ratione excepturi repudiandae distinctio
-        consequatur minima autem ab! Rem, perferendis modi accusamus suscipit
-        repudiandae saepe expedita quisquam voluptatum nihil deleniti et quos
-        quod ratione fuga aut temporibus aliquid!
-      </p>
+      <div class="container">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa,
+          quisquam debitis hic est sit voluptatibus earum temporibus
+          reprehenderit quas praesentium suscipit corrupti ratione excepturi
+          repudiandae distinctio consequatur minima autem ab! Rem, perferendis
+          modi accusamus suscipit repudiandae saepe expedita quisquam voluptatum
+          nihil deleniti et quos quod ratione fuga aut temporibus aliquid!
+        </p>
+      </div>
+    </div>
+    <div class="parallex">
+      <div class="container text-center">
+        <h2 class="text-dark p-5">Some clients & Partners</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          voluptate quisquam sit voluptas a ab laborum assumenda fugit dolor,
+          enim illum quo dicta quas, cum et provident recusandae quos inventore?
+          Dicta nulla voluptates possimus. Suscipit esse, iusto laudantium
+          exercitationem reprehenderit sed cum saepe voluptates error non
+          inventore, nulla quidem dolore. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Ipsam voluptates dignissimos explicabo, ab ut
+          ratione suscipit recusandae debitis labore cumque nemo assumenda
+          laboriosam ad ex quasi sunt, adipisci facere quas cupiditate! Ex
+          eveniet eius nemo libero molestiae dolor impedit veritatis, sequi
+          harum fugiat quibusdam ullam sint autem pariatur, minus cupiditate cum
+          aliquid, eaque dolorem dicta. Nulla, quisquam corporis. Rem cumque
+          voluptatem aliquam, nobis culpa quibusdam iure, eaque atque tempore
+          eveniet ipsa dolores explicabo sapiente nihil consequatur consequuntur
+          eum architecto accusantium velit molestias. Quis repellendus dolor
+          quos, repudiandae, vero sint accusantium laborum minus debitis illum
+          in excepturi eius ullam aliquam sequi.
+        </p>
+        <div class="flex-container">
+          <div class="flex-item-left">
+            <img width="200px" src="https://images.unsplash.com/photo-1605711285791-0219e80e43a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="">
+          </div>
+          <div class="flex-item-right">
+            <img width="200px" src="https://images.unsplash.com/photo-1605711285791-0219e80e43a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="">
+          </div>
+          <div class="flex-item-right">
+            <img width="200px" src="https://images.unsplash.com/photo-1605711285791-0219e80e43a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="">
+          </div>
+          <div class="flex-item-right">
+            <img width="200px" src="https://images.unsplash.com/photo-1605711285791-0219e80e43a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="">
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -102,7 +153,6 @@ Hello world
 .flex-container {
   display: flex;
   flex-direction: row;
-  text-align: center;
   // flex-wrap: wrap;
   .flex-item-left,
   .flex-item-right {
@@ -110,19 +160,33 @@ Hello world
     padding: 10px;
   }
 }
-.flex-container-services{
+.flex-container-services {
   display: flex;
   flex-direction: row;
-  text-align: center;
   flex-wrap: wrap;
-  .flex-item-left, .flex-item-right{
+  .flex-item-left,
+  .flex-item-right {
     flex: 50%;
     padding: 5%;
   }
 }
+.flex-container-head {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  .flex-item-left {
+    font-size: 35px;
+    flex: 80%;
+  }
+  .flex-item-right {
+    flex: 20%;
+  }
+}
 /* Responsive layout - makes a one column-layout instead of two-column layout */
 @media (max-width: 800px) {
-  .flex-container, .flex-container-services {
+  .flex-container,
+  .flex-container-services,
+  .flex-container-head {
     flex-direction: column;
   }
 }
