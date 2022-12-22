738936bba1e82a417e84f488d6e9e5b193135d9d
gauthierjorbel22 <jorbelgauthier@gmail.com>
2022-12-22 22:29:19 +0200

    started implementing background image

---
+++ components/Sponsors.vue
@@ -0,0 +1,36 @@
+<template>
+
+<div class="container">
+          <h1 class="mt-5 mb-5">Sponsors & Partners</h1>
+        <div class="flex-container logos">
+          <div class="flex-item-left">
+            <img width="200px" src="../assets/images/itot.png" alt="ITOT Africa" />
+          </div>
+          <div class="flex-item-right">
+            <img width="200px" src="../assets/images/okademy.png" alt="Okademy" />
+          </div>
+          <div class="flex-item-right">
+            <img width="200px" src="../assets/images/rapid_network.png" alt="Rapid Network" />
+          </div>
+          <div class="flex-item-right">
+            <img width="200px" src="../assets/images/ideal_industries.png" alt="Ideal Industries" />
+          </div>
+          <div class="flex-item-right">
+            <img width="200px" src="../assets/images/electronic_congo.png" alt="Electronic Congo" />
+          </div>
+          <div class="flex-item-right">
+            <img width="200px" src="../assets/images/bunker.png" alt="Bunker" />
+          </div>
+        </div>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -177,34 +177,10 @@
     <div class="parallex text-bg-dark" id="testimonials">
       <div class="container text-center">
         <Testimonial />
-       
-        <div class="container">
-          <h1 class="mt-5 mb-5">Sponsors & Partners</h1>
-        <div class="flex-container logos">
-
-          <div class="flex-item-left">
-            <img width="200px" src="../assets/images/itot.png" alt="ITOT Africa" />
-          </div>
-          <div class="flex-item-right">
-            <img width="200px" src="../assets/images/okademy.png" alt="Okademy" />
-          </div>
-          <div class="flex-item-right">
-            <img width="200px" src="../assets/images/rapid_network.png" alt="Rapid Network" />
-          </div>
-          <div class="flex-item-right">
-            <img width="200px" src="../assets/images/ideal_industries.png" alt="Ideal Industries" />
-          </div>
-          <div class="flex-item-right">
-            <img width="200px" src="../assets/images/electronic_congo.png" alt="Electronic Congo" />
-          </div>
-          <div class="flex-item-right">
-            <img width="200px" src="../assets/images/bunker.png" alt="Bunker" />
-          </div>
-        </div>
-        </div>
-      
       </div>
     </div>
+    <Sponsors />
+
     <!-- Fifth section -->
     <div id="contact" class="contact pb-5 pt-5">
       <div class="container">
